8b7ad0c3e727b0b3eff1e7a5689b77d9590ee223
ArzhB3 <arthur.pagelot@gmail.com>
2019-02-26 15:45:35 +0100

    Creation of Attributes, Getters and Setters for User Entity

--- src/main/java/com/formation/logithur/persistence/entity/User.java
+++ src/main/java/com/formation/logithur/persistence/entity/User.java
@@ -1,5 +1,65 @@
 package com.formation.logithur.persistence.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user")
 public class User {
+	
+	// Attribute's Declaration
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	@Column (name = "id", unique = true, nullable = false)
+	private Long id;
+	
+	@Column (name = "email", length = 60, unique = true, nullable = false)
+	private String email;
+	
+	@Column (name = "nickname", length = 20, unique = true, nullable = false)
+	private String nickname;
+	
+	@Column (name = "password", length = 20, nullable = false)
+	private String password;
+		
+	
+	// Getters And Setters
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
 
 }
