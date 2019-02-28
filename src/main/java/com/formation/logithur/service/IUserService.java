cb404bd88429589916e1a89df99e4d812d390bf6
ArzhB3 <arthur.pagelot@gmail.com>
2019-02-28 15:17:52 +0100

    Creation of method userNew and userModify

--- src/main/java/com/formation/logithur/service/IUserService.java
+++ src/main/java/com/formation/logithur/service/IUserService.java
@@ -1,11 +1,12 @@
 package com.formation.logithur.service;
 
 import com.formation.logithur.dto.UserDto;
+import com.formation.logithur.persistence.repository.UserRepository;
 
 public interface IUserService {
 	
 	UserDto userNew(UserDto user);	
 	
-	UserDto userModify();	
+	UserDto userModify(UserDto userDto, UserRepository userRepo);	
 
 }
