a830f5035350ce7a7e769c9aa98fd5f8038b1fe8
ArzhB3 <arthur.pagelot@gmail.com>
2019-02-28 13:22:02 +0100

    Change Getters and Setters

--- src/main/java/com/formation/logithur/persistence/entity/User.java
+++ src/main/java/com/formation/logithur/persistence/entity/User.java
@@ -71,6 +71,14 @@ public class User {
 	public void setNickname(String nickname) {
 		this.nickname = nickname;
 	}
+	
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 	public Double getMark() {
 		return mark;
