c17a2f44d3351912250bf0b2bffca0e310491c07
Dell <Dell@Dell-PC>
2019-02-28 16:00:59 +0100

    user create

---
+++ src/main/java/com/formation/logithur/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.formation.logithur.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.formation.logithur.dto.UserDto;
+import com.formation.logithur.service.IUserService;
+
+@CrossOrigin
+@RestController
+@RequestMapping(value="/api/client/")
+public class UserController {
+	@Autowired
+	IUserService userServ;
+	
+	@PostMapping
+	@ResponseBody
+	UserDto createUser(UserDto userDto) {
+		return userServ.userNew(userDto);
+	}
+}
