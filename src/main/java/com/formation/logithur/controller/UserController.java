e029dc62d8f5bea263c620663bc32c8c5b84dd28
ArzhB3 <arthur.pagelot@gmail.com>
2019-03-01 13:28:15 +0100

    Delete forgotten Commentary "Head <<<<< "

--- src/main/java/com/formation/logithur/controller/UserController.java
+++ src/main/java/com/formation/logithur/controller/UserController.java
@@ -1,7 +1,7 @@
 package com.formation.logithur.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-<<<<<<< HEAD
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
