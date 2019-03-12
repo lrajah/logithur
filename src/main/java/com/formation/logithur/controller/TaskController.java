458cb67a6bf3a3e1f6c5e6424f46c2ae56506151
Dell <Dell@Dell-PC>
2019-03-12 10:41:35 +0100

    res

--- src/main/java/com/formation/logithur/controller/TaskController.java
+++ src/main/java/com/formation/logithur/controller/TaskController.java
@@ -3,6 +3,7 @@ package com.formation.logithur.controller;
 import java.text.ParseException;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
